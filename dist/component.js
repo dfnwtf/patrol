2fa3f14bfb3e92371c3c1e61eaab05c31b33da46
Department of Financial Nonsense <official@dfn.wtf>
2025-10-05 00:47:14 -0500

    Update component.js

--- dist/component.js
+++ dist/component.js
@@ -1,5 +1,5 @@
 // component.js
-console.log("[DFN Components] v5.2.3 initialized - Final Hybrid Simulation");
+console.log("[DFN Components] v5.2.4 initialized - Final Hybrid Simulation");
 
 function sanitizeHTML(str) {
     if (!str) return '';
